0510b9fbe6189261b56bd87079fc2cb23946121e
Joshua Patterson <joshua.patterson745@gmail.com>
2025-09-05 09:21:30 -0500

    Start basic PoC with query parameters

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def weather_at_location():
+    lat = request.args.get("lat")
+    lon = request.args.get("lon")
+    if lat is None or lon is None:
+        return "HTTP Status 400: Please specify the 'lat' and 'lon' query parameters on this endpoint. E.g. '/?lat=38.8648&lon=-94.6674'", 400
+    return f"{lat}, {lon}"
 
 
 if __name__ == '__main__':
